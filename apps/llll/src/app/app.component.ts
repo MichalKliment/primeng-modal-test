93401b6fb46e134d36d6289204da7d2eff8c5079
Michal Kliment <michal@ralba.co>
2022-11-26 19:43:27 +0100

    Initial commit

---
+++ apps/llll/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'llll-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'llll';
+}
